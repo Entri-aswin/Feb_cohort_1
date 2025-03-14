e9f7e0f6be5c3773fb10666095a5d637572f17a3
Aswinkumar <aswinkumar.cm@entri.me>
2025-03-14 22:07:16 +0530

    day 5

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { PrimaryButton } from "./components/Button";
+
+function App() {
+    const name = "hari";
+    const [count, setCount] = useState(0);
+
+    const updateCount = () => {
+        setCount(count + 1);
+    };
+
+    return (
+        <>
+            <h1>Parent Component</h1>
+            <PrimaryButton count={count} onClick={updateCount} />
+            <PrimaryButton count={count} onClick={updateCount} />
+        </>
+    );
+}
+
+export default App;
